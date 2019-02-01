43fd16a1f5ea99cde7743ccff0242ac1b279964c
Ismail <codeismail@gmail.com>
2019-02-01 07:51:55 +0100

    refactor directory name

--- app/src/main/java/com/idealorb/indimovies/UI/ShowTrackingViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.idealorb.indimovies.UI
-
-import androidx.lifecycle.ViewModel;
-
-class ShowTrackingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/idealorb/indimovies/model/MainModel.kt
+++
@@ -1,4 +0,0 @@
-package com.idealorb.indimovies.model
-
-data class MainModel (val categoryTitle : String,
-                      val tvShows : List<TvShow?>?)
